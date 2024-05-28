74f33c71fd7a5f695c8a1e31dfb2041ca87fd49d
jaylynnwalker <89668672+jaylynnwalker@users.noreply.github.com>
2024-05-27 20:57:24 -0500

    Add files via upload
    Initial upload of files

---
+++ app.js
@@ -0,0 +1,56 @@
+// Targeting items in the html
+const menu = document.querySelector('#mobile-menu') // hamburger icon
+const menuLinks = document.querySelector('.navbar__menu') // navigation bar
+const navLogo = document.querySelector('#navbar__logo')
+
+// Display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const travelMenu = document.querySelector('#travel-page')
+    let scrollPos = window.scrollY // Scroll position
+
+    // Adds the 'highlight' class to the menu items
+    if(window.innerWidth > 960 && scrollPos < 600) { // only want the highlight feature to show on desktop view
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }   else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        travelMenu.classList.remove('highlight')
+        return
+    }   else if (window.innerWidth > 960 && scrollPos < 2345) {
+        travelMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight')
+    }
+}
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+
+// Close mobile menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    // if view is mobile and menu is open
+    if (window.innerWidth <= 768 && menuBars) {    
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
